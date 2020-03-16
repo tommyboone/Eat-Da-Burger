8c87931f649c11cf2b5751adc9aba60897254dae
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-03-16 10:51:53 -0500

    database created and modules exported

---
+++ config/orm.js
@@ -0,0 +1,8 @@
+const connection = require("./connection.js");
+
+let orm = {
+
+    
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+  
+-- USE burgers_db;
+
+-- INSERT INTO burgers (burger_name, devoured, date)
+-- VALUES
+-- ( "Cheese Burger", FALSE, null),
+-- ( "Bacon Burger", FALSE, null),
+-- ( "Quater Pounder Burger", FALSE, null),
+-- ( "BBQ Burger", FALSE, null),
+-- ( "Triple Decker Burger", FALSE, null),
+-- ( "Impossible Burger", FALSE, null);
