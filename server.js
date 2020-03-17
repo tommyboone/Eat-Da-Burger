961a48c8b686c4e8538a2daed732d118d3495281
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-03-17 13:16:07 -0500

    error fixes

--- server.js
+++ server.js
@@ -1,2 +1,28 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars')
+const routes = require("./controllers/burgers_controller.js");
+var bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
