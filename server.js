0d47f672320683ea0ec5a0e84b1270c09c41a166
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-03-18 09:51:31 -0500

    fixed an error that wasn't allowing the frontend.js file to run in browser

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use('/', express.static(__dirname, + "public"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
