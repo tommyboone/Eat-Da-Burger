9af36be036f06db25c1ed03e21539a4bfe1db12e
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-03-19 17:21:38 -0500

    fixed typo

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const exphbs = require('express-handlebars')
+const exphbs = require("express-handlebars");
 const routes = require("./controllers/burgers_controller.js");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use('/', express.static(__dirname, + "public"));
+app.use("/", express.static(__dirname, +"public"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,10 +19,8 @@ app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
-
 app.use(routes);
 
 app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+  console.log("Server listening on: http://localhost:" + PORT);
 });
